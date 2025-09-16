5ecd94dec8989912c9ed825c6e9e8b1b47e0626a
JP LPF <127799682+Reidosrato@users.noreply.github.com>
2025-09-16 00:41:48 -0300

    Delete CorridaBaratas directory

--- CorridaBaratas/src/main/java/corridabaratas/corridabaratas/repository/LeaderboardsRepository.java
+++
@@ -1,9 +0,0 @@
-package corridabaratas.corridabaratas.repository;
-
-import corridabaratas.corridabaratas.entity.Leaderboards;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface LeaderboardsRepository extends JpaRepository<Leaderboards, Integer> {
-}
--- CorridaBaratas/src/main/java/corridabaratas/corridabaratas/repository/TimeRepository.java
+++
@@ -1,9 +0,0 @@
-package corridabaratas.corridabaratas.repository;
-
-import corridabaratas.corridabaratas.entity.Time;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface TimeRepository extends JpaRepository<Time, Integer> {
-}
